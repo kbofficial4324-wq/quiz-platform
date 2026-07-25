cf210337e2341f9f8b18d3609c7d5b4d82c9fc0d
kbofficial4324-wq <kbofficial4324@gmail.com>
2026-07-25 10:54:10 +0530

    Updated authentication and deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,4 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // ======================
@@ -26,13 +25,12 @@ export const register = async (req, res) => {
     }
 
     // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create new user
     const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password,
       role: role || "student",
     });
 
@@ -71,16 +69,11 @@ export const login = async (req, res) => {
     }
 
     // Compare password
-    const isMatch = await bcrypt.compare(
-      password,
-      user.password
-    );
-
-    if (!isMatch) {
-      return res.status(400).json({
-        message: "Invalid email or password",
-      });
-    }
+   if (password !== user.password) {
+    return res.status(400).json({
+        message: "Invalid credentials"
+    });
+}
 
     // Create JWT token
     const token = jwt.sign(
